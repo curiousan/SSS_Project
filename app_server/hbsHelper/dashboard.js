386bb97c648901a5378105e26f5e73b183082c30
naren <blazeraze.naren@gmail.com>
2019-05-04 18:22:38 +0300

    updated css for own video section

--- app_server/hbsHelper/dashboard.js
+++ app_server/hbsHelper/dashboard.js
@@ -12,16 +12,16 @@ const hbsHelpers = (hbs) => {
       ownVideo: (currentUser, mediaOwner) => {
         const username = currentUser.facebook.email || currentUser.local.email;
         if (mediaOwner === username) {
-          return '<button  type="button" class="btn btn-sm btn-outline-secondary btn-video-edit">Edit</button><button type="button" class="btn btn-sm btn-outline-secondary btn-video-delete">delete</button>';
+          return '<button  type="button" class="btn btn-sm btn-outline-info btn-video-edit mr-2">Edit</button><button type="button" class="btn btn-sm btn-outline-dark btn-video-delete">delete</button>';
         }
         return '';
       },
       formatDateTime: (date) => {
         return dayjs(date).format('DD MMM, YYYY');
       },
-      fixDesc: (string) =>{
-        return string.substring(0,27).concat('..');
-      }
+      fixDesc: (string) => {
+        return string.substring(0, 27).concat('..');
+      },
     },
   });
 };
