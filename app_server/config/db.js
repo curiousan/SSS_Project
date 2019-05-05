628cbe5574e3ca6ef3fdcaea2312975b1efd5db0
naren <blazeraze.naren@gmail.com>
2019-05-05 17:32:20 +0300

    facebook auth configured with new ID; fixed navbar color change in page refresh

--- app_server/config/db.js
+++ app_server/config/db.js
@@ -1,19 +1,38 @@
 'use strict';
 
-
 class Database {
   constructor() {
     this.mongoose = require('mongoose');
+    // if (process.env.NODE_ENV === 'development') {
+    //   this.https = require('https');
+    //   const fs = require('fs');
+    //   const sslkey = fs.readFileSync('ssl-key.pem');
+    //   const sslcert = fs.readFileSync('ssl-cert.pem');
+    //   this.options = {
+    //     key: sslkey,
+    //     cert: sslcert,
+    //   };
+    // }
   }
 
-
-  connect(url, app, http) {
-    this.mongoose.connect(url).then(() => {
-      console.log('Connected successfully.');
-      http.createServer(app).listen(process.env.PORT|| 3000);
-    }, (err) => {
-      console.log('Connection to db failed: ' + err);
-    });
+  connect(url, app, port) {
+    this.mongoose.connect(url).then(
+      () => {
+        app.listen(port, () => console.log('Connected successfully.'));
+        // if (process.env.NODE_ENV === 'development') {
+        //   this.https.createServer(this.options, app).listen(port, () => {
+        //     console.log(`server running on port ${port}`);
+        //   });
+        // } else {
+        //   return app.listen(port, () => {
+        //     console.log(`server started at ${port}`);
+        //   });
+        // }
+      },
+      (err) => {
+        console.log('Connection to db failed: ' + err);
+      }
+    );
   }
 
   getSchema(schema, name) {
